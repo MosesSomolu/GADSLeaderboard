b2320160eb187754782cb6bada7b98491d61eaa4
MosesSomolu <66791254+MosesSomolu@users.noreply.github.com>
2020-09-08 16:29:20 +0100

    Added RecyclerView Adapters to Fragment Activities

--- app/src/main/java/com/example/gadsleaderboard/LearnersFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/LearnersFragment.kt
@@ -1,10 +1,12 @@
 package com.example.gadsleaderboard
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 // Fragment linked to MainActivity and Viewpager to display data on the Top learners
 class LearnersFragment : Fragment() {
@@ -24,6 +26,25 @@ class LearnersFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment with its respective xml file.
         return inflater.inflate(R.layout.fragment_leaders, container, false)
+    }
+
+    internal inner class LearnersAdapter(context : Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+    internal inner class ViewHolder(){
+
     }
 
 }
--- app/src/main/java/com/example/gadsleaderboard/SkillFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/SkillFragment.kt
@@ -1,10 +1,12 @@
 package com.example.gadsleaderboard
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 // Fragment linked to MainActivity and Viewpager to display data on the top Skill IQ learners
 class SkillFragment : Fragment() {
@@ -24,6 +26,26 @@ class SkillFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment with its respective xml file.
         return inflater.inflate(R.layout.fragment_skill, container, false)
+    }
+
+    internal inner class SkillAdapter(context : Context) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+    internal inner class ViewHolder(){
+
     }
 
 }
